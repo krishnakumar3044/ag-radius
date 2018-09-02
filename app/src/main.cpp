06cadf2f93f54a2e39f959ebcfd83fc0e9ea3361
krishnakumar3044 <krishna.kumar@globaleagle.com>
2018-09-02 00:59:23 -0700

    - Add radius library

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -37,7 +37,7 @@ sendAuthRequest()
 
     currAttribute.attributeId          = PW_SERVICE_TYPE;
     currAttribute.attributeType        = PW_TYPE_INTEGER;    
-    currAttribute.attributeIntValue    = PW_AUTHENTICATE_ONLY;
+    currAttribute.attributeIntValue    = PW_LOGIN;
     sendAttributeList->push_back(currAttribute);
 
     currAttribute.attributeId          = PW_NAS_IP_ADDRESS;
@@ -48,24 +48,53 @@ sendAuthRequest()
     currAttribute.attributeIntValue = intNorder;
     sendAttributeList->push_back(currAttribute);    
 
-    cout << "send size: " << sendAttributeList->size() << "\n";
-    cout << "30: receive size: " << receiveAttributeList->size() << "\n";
+    currAttribute.attributeId          = PW_NAS_PORT;
+    currAttribute.attributeType        = PW_TYPE_INTEGER;    
+    currAttribute.attributeIntValue    = 0;
+    sendAttributeList->push_back(currAttribute);
+
+    currAttribute.attributeId          = PW_CALLED_STATION_ID;
+    currAttribute.attributeType        = PW_TYPE_STRING;
+    currAttribute.attributeStringValue = "00-50-E8-00-99-20";
+    currAttribute.vendorpec            = VENDOR_NONE;      
+    sendAttributeList->push_back(currAttribute);
+
+    currAttribute.attributeId          = PW_CALLING_STATION_ID;
+    currAttribute.attributeType        = PW_TYPE_STRING;
+    currAttribute.attributeStringValue = "E0-B5-2D-AF-1C-50";
+    currAttribute.vendorpec            = VENDOR_NONE;      
+    sendAttributeList->push_back(currAttribute);
+
+    currAttribute.attributeId          = NOMADIX_LOGOFF_URL;
+    currAttribute.attributeType        = PW_TYPE_STRING;    
+    currAttribute.attributeStringValue = "http://1.1.1.1";
+    currAttribute.vendorpec            = VENDOR_NOMADIX;    
+    sendAttributeList->push_back(currAttribute);   
+
+    currAttribute.attributeId          = PW_NAS_IDENTIFIER;
+    currAttribute.attributeType        = PW_TYPE_STRING;    
+    currAttribute.attributeStringValue = "AG2400";
+    currAttribute.vendorpec            = VENDOR_NONE;    
+    sendAttributeList->push_back(currAttribute);   
 
     result = agrc->sendAuthRequest(AccessGatewayRadiusClient::AGRC_AUTH_REQUEST, 
                                    sendAttributeList,
                                    receiveAttributeList);
-    cout << "57: result: " << result << "\n";                          
+    cout << "result: " << result << "\n";                          
 
-    cout << "59: receive size: " << receiveAttributeList->size() << "\n";                          
+    cout << "Number of received attributes: " << receiveAttributeList->size() << "\n";                          
 
     // Process all the received attributes
     for (receiveIt = (*receiveAttributeList).begin(); receiveIt != (*receiveAttributeList).end(); ++receiveIt)
     {
-        cout << "Attribute id: " << receiveIt->attributeId << "\n";
-        cout << "Attribute String value: " << receiveIt->attributeStringValue << "\n";        
+        cout << "Receive Attribute id           : " << receiveIt->attributeId << "\n";
+        cout << "Receive Attribute name         : " << receiveIt->attributeStringName << "\n";               
+        cout << "Receive Attribute type         : " << receiveIt->attributeType << "\n";           
+        cout << "Receive Attribute int value    : " << receiveIt->attributeIntValue << "\n";                        
+        cout << "Receive Attribute String value : " << receiveIt->attributeStringValue << "\n";        
+        cout << "Receive Attribute vendorpec    : " << receiveIt->vendorpec << "\n";                
         cout << "\n";           
     }
-
 
     //Delete all the send attributes
     sendAttributeList->erase(sendAttributeList->begin(), sendAttributeList->end());
@@ -101,16 +130,7 @@ sendAcctRequest()
     currAttribute.attributeType        = PW_TYPE_INTEGER;    
     currAttribute.attributeIntValue    = 1;
     currAttribute.vendorpec            = VENDOR_NOMADIX;    
-    sendAttributeList->push_back(currAttribute);
-
-    cout << "send size: " << sendAttributeList->size() << "\n";
-    cout << "30: receive size: " << receiveAttributeList->size() << "\n";
-
-/*
-    agrc->sendAuthRequest(AccessGatewayRadiusClient::AGRC_AUTH_REQUEST, 
-                          sendAttributeList,
-                          receiveAttributeList);
-*/                          
+    sendAttributeList->push_back(currAttribute);                     
 
     result = agrc->sendAcctRequest(AccessGatewayRadiusClient::AGRC_ACCT_REQUEST, 
                                    sendAttributeList);
